9ed273d8652c4c7b585076167cec585c0075e422
Jeison Tomio <medriase@gmail.com>
2024-08-09 16:53:17 -0300

    :sparkles: feat: created pages and fix api email

---
+++ src/app/participate/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div className="container">
+      <div className="p-4">
+      <h1 className="text-center">Entre na brincadeira</h1>
+      </div>
+    </div>
+  )
+}
---
+++ src/app/wishlist/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div className="container">
+      <div className="p-4">
+      <h1 className="text-center">Confira lista desejo</h1>
+      </div>
+    </div>
+  )
+}
